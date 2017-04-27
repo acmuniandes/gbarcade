93443c7b712d3f0adf78ff02c12e60993ef1dded
birucan <birucandev@gmail.com>
2017-04-26 20:07:20 -0500

    pong finished, just minor details needed

--- pong.c
+++ pong.c
@@ -15,12 +15,13 @@ void loadSprites();
 void startBall();
 void updateSprite(char pad, signed char cuanto);
 void moveBall();
+bool collisionCheck(unsigned char x1, unsigned char y1, unsigned char w1, unsigned char h1, unsigned char x2, unsigned char y2, unsigned char w2, unsigned char h2);
 
 //paddle 1 position on Y axis
-unsigned char PosPad1=70;
+unsigned char yPad1=70;
 
 //paddle 2 position on Y axis
-unsigned char PosPad2=70;
+unsigned char yPad2=70;
 
 unsigned char countPad=0;
 
@@ -34,8 +35,8 @@ int count=0;//Delays the ball without halt the whole system
 
 int i;
 //Extra
-unsigned char score1=0;
-unsigned char score2=0;
+UINT8 score1=0;
+UINT8 score2=0;
 //pointer to memory address for random number generation 
 unsigned char *ptr_div_reg = 0xFF04;
 unsigned char randy = *(ptr_div_reg); 
@@ -66,12 +67,12 @@ void loadSprites(){
 	//this one is the ball
 	set_sprite_tile(4,4);
 	//sets pad1(right paddle)
-	move_sprite(0, 150, PosPad1);
-	move_sprite(1, 150, PosPad1+8);
+	move_sprite(0, 150, yPad1);
+	move_sprite(1, 150, yPad1+8);
 
 	//sets pad2(left paddle)
-	move_sprite(2, 20, PosPad2);
-	move_sprite(3, 20, PosPad2+8);
+	move_sprite(2, 20, yPad2);
+	move_sprite(3, 20, yPad2+8);
 
 	//sets ball
 	move_sprite(4, xBall, yBall); 
@@ -79,17 +80,30 @@ void loadSprites(){
 	DISPLAY_ON;
 	SHOW_SPRITES;
 }
+
+bool collisionCheck(unsigned char x1, unsigned char y1, unsigned char w1, unsigned char h1, unsigned char x2, unsigned char y2, unsigned char w2, unsigned char h2){
+		if ((x1 < (x2+w2)) && ((x1+w1) > x2) && (y1 < (h2+y2)) && ((y1+h1) > y2)) {
+		
+		return true;
+	
+	}else {
+		
+		return false;
+	
+}
+}
+
 void updateSprite(char pad, signed char cuanto){
 
 	if(pad==1){
-	PosPad1+=cuanto;
-	move_sprite(0, 150, PosPad1);
-	move_sprite(1, 150, PosPad1+8);
+	yPad1+=cuanto;
+	move_sprite(0, 150, yPad1);
+	move_sprite(1, 150, yPad1+8);
 	}
 	if(pad==2){
-	PosPad2+=cuanto;
-	move_sprite(2, 20, PosPad2);
-	move_sprite(3, 20, PosPad2+8);
+	yPad2+=cuanto;
+	move_sprite(2, 20, yPad2);
+	move_sprite(3, 20, yPad2+8);
 	}
 	return;
 }
@@ -107,19 +121,29 @@ void moveBall(){
 		score2++;
 		newBall=true;
 	}
+	if(collisionCheck(150, yPad1, 8,16, xBall, yBall, 8,8)==true || collisionCheck(20, yPad2, 8,16, xBall, yBall, 8,8)==true){
+	xVel=xVel*-1;
+		if(collisionCheck(150, yPad1, 8,2, xBall, yBall, 8,8)==true|| collisionCheck(20, yPad2, 8,2, xBall, yBall, 8,8)==true){
+			if(yVel==0){
+				yVel=1;
+			}else{
+				yVel=yVel*-1;
+			}
+		}
+		
+	}
+
 	count++;
-	if(count==40){
+	if(countPad==40){
+		up:
+		countPad++;
+		if(countPad!=80){
+			goto up;
+		}
 	xBall=xBall+xVel;
 	yBall=yBall+yVel;
 	move_sprite(4, xBall, yBall);
 	count=0;
-	}
-	//collision check
-	if(xBall==150 || xBall==151){
-		if(yBall>=PosPad1 && yBall<=PosPad1+16){
-			xVel=xVel*-1;
-		}
-		
 	}
 	  
 	
@@ -128,7 +152,9 @@ void startBall(){
 	xBall=80;
 	yBall=72;
 	move_sprite(4, 80, 72);
+	printf("    ""%i", score1,"    ""%i", score2);
 	while(count!=8000){
+		
 		count++;
 	}
 	count=0;
@@ -143,52 +169,43 @@ void startBall(){
 }
 void checkControls(){
 	countPad++;
-	if(countPad==30){
-			if(joypad()==J_UP ){
-			if(PosPad1<=16){
-				//delay(5);	
+	
+	if(countPad==40){
+		if(joypad()==J_UP ){
+			if(yPad1<=16){
 			}else{
 				updateSprite(1, -1);
-				//delay(5);
-			}
+			 } 
 		}
 			
 		
 		if(joypad()==J_DOWN){
-			if(PosPad1>=144){
-				//delay(5);	
+			if(yPad1>=144){
 			}else{
 				updateSprite(1, 1);
-				//delay(5);
-			}
+			 }
 		}
 			
 		
 		if(joypad()==J_A){
-		if(PosPad2>=144){
-		//delay(5);	
+			if(yPad2>=144){	
 			}else{
 				updateSprite(2, 1);
-				//delay(5);
+			 }
 			}
-		}
 
 		if(joypad()==J_B){
-			if(PosPad2<=16){
-				//delay(5);	
+			if(yPad2<=16){
 			}else{
 				updateSprite(2, -1);
-				//delay(5);
-			}
+			 }
 			
 		}
+		moveBall();
 		countPad=0;
 	
 	}
-
-
-			moveBall();
-			return;
+		return;
 }
 
 		
